bca85a474a351f3aa47fb4e92cfd121bd3b87d9f
xupeng <xupeng@droi.com>
2018-01-25 11:43:22 +0800

    add contextlibTest.py

---
+++ pycharm/contextlibTest.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+
+from contextlib import contextmanager
+
+from contextlib import closing
+from urllib.request import urlopen
+
+
+class Query(object):
+    def __init__(self, name):
+        self.name = name
+
+    def query(self):
+        print('Query info about %s...' % self.name)
+
+
+@contextmanager
+def create_query(name):
+    print('Begin')
+    qt = Query(name)
+    yield qt
+    print('End')
+
+
+if __name__ == '__main__':
+    with create_query('Bob') as q:
+        q.query()
+
+    with closing(urlopen('https://www.python.org')) as page:
+        for line in page:
+            print(line)
