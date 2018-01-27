b67a3094293d2c411168f1d614fa79b4ad7a1337
xupeng <xupeng@droi.com>
2018-01-27 15:13:25 +0800

    modiyf urllibtest.py

--- urllibtest.py
+++ urllibtest.py
@@ -1,12 +1,13 @@
 from urllib import request
+import json
 
+'''
 with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
     data = f.read()
     print('Status:', f.status, f.reason)
     for k, v in f.getheaders():
         print('%s: %s' % (k, v))
     print('Data:', data.decode('utf-8'))
-
 
 req = request.Request('http://www.douban.com/')
 req.add_header('User-Agent',
@@ -16,3 +17,23 @@ with request.urlopen(req) as f:
     for k, v in f.getheaders():
         print('%s: %s' % (k, v))
     print('Data:', f.read().decode('utf-8'))
+'''
+
+
+def fetch_data(url):
+    req = request.Request(url)
+    with request.urlopen(req) as f:
+        tmp = f.read()
+        for k, v in f.getheaders():
+            print("key:{}, value:{}".format(k, v))
+        print('Data: ', tmp.decode('utf-8'))
+    data_dct = json.loads(tmp.decode('utf-8'))
+    print('data_dct:', data_dct)
+    return data_dct
+
+
+URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+data = fetch_data(URL)
+print(data)
+assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+print('ok')
--- zhihu.py
+++ zhihu.py
@@ -1,6 +1,7 @@
-import requests
-import time
 import random
+import time
+
+import requests
 
 
 class SendData:
@@ -8,10 +9,14 @@ class SendData:
     def __init__(self):
         self.url = 'https://www.zhihu.com/api/v4/message'
         self.cookie = {}  # 登录需要cookie
-        self.header = {}  # useragent 伪装成浏览器
+        # User-Agent 伪装成浏览器
+        self.header = {
+            'User-Agent',
+            'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25'
+        }
 
-    def send(self, dat):
-        self.html = requests.post(self.url, json=dat, cookies=self.cookie, header=self.header)
+    def send(self, data):
+        self.html = requests.post(self.url, json=data, cookies=self.cookie, header=self.header)
         print(self.html.status_code)
         print(self.html.text)
 
@@ -27,7 +32,7 @@ if __name__ == "__main__":
     '''
     with open('filepath') as f:
         for hash in f:
-            time.sleep(random.randint(15,30))
+            time.sleep(random.randint(15, 30))
             hash = hash.strip('\n')
             dat = {
                 'type': 'common',
