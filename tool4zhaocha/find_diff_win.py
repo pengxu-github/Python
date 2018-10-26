4f504024fe33fd5a592c69058ad92da46e620a1f
xupeng <bearxup@126.com>
2018-10-26 10:08:12 +0800

    1, add test pyplot script for matplotlib backend 2, get mouse position in find diff, and get diff when second picture grabbed issues known: 1, pyplot.show() cause python exception 2, codes after start_get_mouse_click_pos() can not run

--- tool4zhaocha/find_diff_win.py
+++ tool4zhaocha/find_diff_win.py
@@ -4,9 +4,9 @@ import time
 import numpy
 import win32gui
 import matplotlib.pyplot as plt
+import pyHook
+import pythoncom
 from PIL import ImageGrab, ImageChops
-
-from tool4zhaocha.mouse_position import start_get_mouse_click_pos, get_grab_pos
 
 __author__ = 'XuPeng'
 
@@ -27,6 +27,10 @@ img_bottom_bound = 685
 limg_right_bound = 310
 rimg_left_bound = 330
 
+index = 1
+old_x = old_y = 0
+left_l = left_t = left_r = left_b = right_l = right_t = right_r = right_b = 0
+
 
 def gbk2utf8(s):
     return s.decode('gbk').encode('utf-8')
@@ -44,11 +48,12 @@ def show_window_attr(hWnd):
     title = win32gui.GetWindowText(hWnd)
     # title = gbk2utf8(title)
     clsname = win32gui.GetClassName(hWnd)
+    global debug
     if debug:
-        print('-----------------------------')
         print('窗口句柄:%s ' % hWnd)
         print('窗口标题:%s' % title)
         print('窗口类名:%s' % clsname)
+        print('-----------------------------')
 
 
 def show_windows(hWndList):
@@ -69,7 +74,7 @@ def _MyCallback(hwnd, extra):
 def get_hwnds():
     windows = {}
     win32gui.EnumWindows(_MyCallback, windows)
-    print("Enumerated a total of  windows with %d classes".format(len(windows)))
+    print("Enumerated a total of  windows with {} classes".format(len(windows)))
     if debug:
         print('------------------------------')
         # print classes
@@ -87,17 +92,21 @@ def demo_child_windows(parent):
     if not parent:
         return
 
-    hWndChildList = []
-    win32gui.EnumChildWindows(parent, lambda hWnd, param: param.append(hWnd), hWndChildList)
-    show_windows(hWndChildList)
-    return hWndChildList
+    wnd_child_list = []
+    win32gui.EnumChildWindows(parent, lambda hWnd, param: param.append(hWnd), wnd_child_list)
+    show_windows(wnd_child_list)
+    return wnd_child_list
 
 
 def find_child_window(class_name, title_name):
     # 获取句柄
     hwnd = win32gui.FindWindow(class_name, title_name)
+    if not hwnd:
+        print('can not find window: %s, %s ' % (class_name, title_name))
+        return 0
     # 获取窗口左上角和右下角坐标
     left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    global debug
     if debug:
         print('find window from class name: %s, title name: %s, result: %d, %d, %d, %d'
               % (class_name, title_name, left, top, right, bottom))
@@ -152,29 +161,37 @@ def find_diff(left_l=img_left_bound, left_t=img_top_bound, right_r=img_right_bou
     :param right_l: begin of right picture, in the picture grabbed
     :return: NULL
     """
-    hWndList = get_hwnds()
-    show_windows(hWndList)
+    print('src image: %d, %d, %d, %d, left_width: %d, right_l: %d'
+          % (left_l, left_t, right_r, right_b, limg_right_bound, right_l))
+    wnd_list = get_hwnds()
+    show_windows(wnd_list)
 
-    wnd_id, l, t, r, b = find_child_window(wnd_class, wnd_title)
-    print('windows id, left, top, right, bottom: %d, %d, %d, %d, %d' % (wnd_id, l, t, r, b))
-    # full windows display
-    # win32gui.ShowWindow(wnd_id, win32con.SW_MAXIMIZE)
-    # bring window to foreground
-    win32gui.SetForegroundWindow(wnd_id)
+    wnd_id = find_child_window(wnd_class, wnd_title)
+    if not wnd_id == 0:
+        wnd_id, l, t, r, b = wnd_id
+        print('windows id, left, top, right, bottom: %d, %d, %d, %d, %d' % (wnd_id, l, t, r, b))
+        # full windows display
+        # win32gui.ShowWindow(wnd_id, win32con.SW_MAXIMIZE)
+        # bring window to foreground
+        win32gui.SetForegroundWindow(wnd_id)
 
-    time.sleep(3)
-    src_image = ImageGrab.grab((left_l, left_t, right_r, right_b))
+        time.sleep(3)
+    else:
+        print('scr image: {}, {}, {}, {}'.format(left_l, left_t, right_r, right_b))
+        src_image = ImageGrab.grab((left_l, left_t, right_r, right_b))
     # do not use src_image.show() here,
     # because show this picture will influence left img and right img cut
     width, height = src_image.size
     # cut left img and right img
-    left_box = (0, 1, left_width, height)
+    print('left image: 0, 0, {}, {}'.format(left_width, height))
+    print('right image: {}, 0, {}, {}'.format(right_l, left_width, height))
+    left_box = (0, 0, left_width, height)
     right_box = (right_l, 0, width, height)
     image_left = src_image.crop(left_box)
     image_right = src_image.crop(right_box)
     # image_left.show()
     # image_right.show()
-
+    '''
     plt.figure(num='image diff', figsize=(10, 7))
     plt.suptitle('image diff')
     plt.subplot(1, 3, 1), plt.title('image_left')
@@ -188,18 +205,59 @@ def find_diff(left_l=img_left_bound, left_t=img_top_bound, right_r=img_right_bou
     plt.subplot(1, 3, 3), plt.title('image_diff')
     plt.imshow(img_diff)
     plt.show()
+    '''
+    diff = ImageChops.difference(image_left, image_right)
+    diff.show()
+
+
+def on_mouse_event(event):
+    global hm, index, left_l, left_t, left_r, left_b, right_l, right_t, right_r, right_b, old_x, old_y
+    print('event.MessageName: %s' % event.MessageName)
+    if event.MessageName == "mouse left down":
+        old_x, old_y = event.Position
+        print('click down: %d, %d' % (old_x, old_y))
+    if event.MessageName == "mouse left up":
+        new_x, new_y = event.Position
+        print('click up: %d, %d' % (new_x, new_y))
+        # image = ImageGrab.grab((old_x, old_y, new_x, new_y))
+        # image.show()
+        if index == 1:
+            left_l, left_t, left_r, left_b = (old_x, old_y, new_x, new_y)
+            index = index - 1
+        elif index == 0:
+            right_l, right_t, right_r, right_b = (old_x, old_y, new_x, new_y)
+            hm.UnhookMouse()
+            hm.UnhookKeyboard()
+            hm = None
+            print('left: %d, %d, %d, %d; right: %d, %d, %d, %d'
+                  % (left_l, left_t, left_r, left_b, right_l, right_t, right_r, right_b))
+            find_diff(left_l, left_t, right_r, right_b, left_r - left_l, right_l - left_l)
+    return True
+
+
+def start_get_mouse_click_pos():
+    global hm, index
+    index = 1
+    hm = pyHook.HookManager()
+    hm.SubscribeMouseAllButtons(on_mouse_event)
+    hm.HookMouse()
+    hm.HookKeyboard()
+    pythoncom.PumpMessages()
 
 
 def start_grab():
-    print("please click upper left corner of the left diff picture")
+    # global old_x, old_y, new_x, new_y
+    print("依次截取需要对比的两张图片：")
     start_get_mouse_click_pos()
-    left_l, left_t, left_r, left_b = get_grab_pos()
-    print("l, t, r, b: %d, %d, %d, %d" % (left_l, left_t, left_r, left_b))
-    print("please click upper left corner of the right diff picture")
-    start_get_mouse_click_pos()
-    right_l, right_t, right_r, right_b = get_grab_pos()
 
-    find_diff(left_l, left_t, right_r, right_b, left_r - left_l, right_l - left_r)
+    # codes from this line can not execute, maybe because of:
+    # 1, pythoncom.PumpMessages
+    # 2, hm.UnhookMouse() & hm.UnhookKeyboard()
+    # print("l, t, r, b: %d, %d, %d, %d" % (old_x, old_y, new_x, new_y))
+
+    # print("please grab the right diff picture")
+    # start_get_mouse_click_pos()
+    # print("l, t, r, b: %d, %d, %d, %d" % (old_x, old_y, new_x, new_y))
 
 
 if __name__ == "__main__":
