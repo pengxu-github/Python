0db18a76cb0f71c905ea2686b8c43701bfcd0f45
xupeng <bearxup@126.com>
2018-11-13 17:12:02 +0800

    change log print to chinese

--- test/remain.py
+++ test/remain.py
@@ -17,16 +17,16 @@ def month_pay(all_lend):
 def remain_10m(lend_money):
     lend_money = lend_money * 10000
     month_pay_all = month_pay(lend_money)
-    print('lend {}, month pay: {}'.format(lend_money, month_pay_all))
-    print('\tmonth remain: {}'.format(month - month_pay_all))
-    print('\tafter 10 month remain: {}'.format(10 * (month - month_pay_all)))
-    print('\tafter 10 month, total remain: {}'.format(
+    print('贷款 {}, 月还: {}'.format(lend_money, month_pay_all))
+    print('\t每月剩余: {}'.format(month - month_pay_all))
+    print('\t10个月之后剩余: {}'.format(10 * (month - month_pay_all)))
+    print('\t10个月之后总共剩余: {}'.format(
         lend_money * (1 - lend_cost) - must_pay_car - must_pay_gu - must_pay_jj + 10 * (month - month_pay_all)))
 
 
 def all_interest(lend_money):
     all_interest_money = lend_money * lend_cost + lend_money * lend_rate_monty * lend_years * 12
-    print("\tlend {}w, 5 years all interest is: {}w\n".format(lend_money, all_interest_money))
+    print("\t贷款 {}w, 5年总利息: {}w\n".format(lend_money, all_interest_money))
 
 
 if __name__ == "__main__":
