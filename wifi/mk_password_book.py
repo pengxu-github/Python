c998d3971a0340641bbe0d8dfbe0e2163902f303
xupeng <xupeng@droi.com>
2019-03-27 15:29:43 +0800

    [20190327] refact make password

--- wifi/mk_password_book.py
+++ wifi/mk_password_book.py
@@ -1,22 +1,36 @@
 # coding:utf-8
+import datetime
 import itertools as its
+import string
+
+import os
+
+password_file = "password.txt"
 
 
 def mk_pas():
     # 迭代器
-    words = "1234567890"
+    digits = string.digits
+    alpha = string.ascii_letters
+    words = digits + alpha
+    print(words)
     # 生成密码本的位数，五位数，repeat=5
-    r = its.product(words, repeat=1)
-    # 保存在文件中，追加
-    dic = open("password.txt", "a")
-    # i是元组
-    for i in r:
-        # jion空格链接
-        dic.write("".join(i))
-        dic.write("".join("\n"))
-        print(i)
-    dic.close()
-    print("密码本已生成")
+    r = its.product(words, repeat=6)
+    time = datetime.datetime.now()
+    new_file = "password-" + time.strftime('%Y%m%d%H%M%S') + ".txt"
+    print(new_file)
+    if os.path.exists(password_file):
+        os.rename(password_file, new_file)
+    print("begin make password book")
+    with open(password_file, "wt") as f:
+        # i是元组
+        for i in r:
+            # jion空格链接
+            f.write("".join(i))
+            f.write("".join("\n"))
+            # print(i)
+    delta_time = datetime.datetime.now() - time
+    print("make password book success, use %sms" % delta_time)
 
 
 if __name__ == "__main__":
